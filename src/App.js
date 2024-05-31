79bcc0fe29e4f9c6487b6b210c00a0c4b209a7fc
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-05-31 13:16:11 +0530

    changes in app.js

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import './App.css';
+
+function App() {
+    return (
+        <main>
+            <header>
+                <a href="" className="logo">MyBlog</a>
+                <nav>
+                    <a href="">Login</a>
+                    <a href="">Register</a>
+                </nav>
+            </header>
+            <div className="post">
+                <div className="image">
+                    <img
+                        src="https://ichef.bbci.co.uk/news/1024/cpsprodpb/5496/live/49fdc940-1d9b-11ef-a090-6b563b2f66e3.jpg.webp"
+                        alt=""/>
+                </div>
+                <div className="texts">
+                    <h2>Call of Duty to go to Game Pass in gaming shake-up</h2>
+                    <p className="info">
+                        <a className="author">Dawid Paszko</a>
+                        <time>2024-5-29 23:04</time>
+                    </p>
+                    <p className="summary">Microsoft has torn up the rules of big video game launches by announcing its most eagerly awaited
+                        new title - Call of Duty: Black Ops 6 </p>
+                </div>
+            </div>
+            <div className="post">
+                <div className="image">
+                    <img
+                        src="https://ichef.bbci.co.uk/news/1024/cpsprodpb/5496/live/49fdc940-1d9b-11ef-a090-6b563b2f66e3.jpg.webp"
+                        alt=""/>
+                </div>
+                <div className="texts">
+                    <h2>Call of Duty to go to Game Pass in gaming shake-up</h2>
+                    <p className="info">
+                        <a className="author">Dawid Paszko</a>
+                        <time>2024-5-29 23:04</time>
+                    </p>
+                    <p className="summary">Microsoft has torn up the rules of big video game launches by announcing its most eagerly awaited
+                        new title - Call of Duty: Black Ops 6 </p>
+                </div>
+            </div>
+            <div className="post">
+                <div className="image">
+                    <img
+                        src="https://ichef.bbci.co.uk/news/1024/cpsprodpb/5496/live/49fdc940-1d9b-11ef-a090-6b563b2f66e3.jpg.webp"
+                        alt=""/>
+                </div>
+                <div className="texts">
+                    <h2>Call of Duty to go to Game Pass in gaming shake-up</h2>
+                    <p className="info">
+                        <a className="author">Dawid Paszko</a>
+                        <time>2024-5-29 23:04</time>
+                    </p>
+                    <p className="summary">Microsoft has torn up the rules of big video game launches by announcing its most eagerly awaited
+                        new title - Call of Duty: Black Ops 6 </p>
+                </div>
+            </div>
+
+        </main>
+    );
+}
+
+export default App;
